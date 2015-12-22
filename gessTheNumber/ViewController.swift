206031895f4c44c5106fbcdea97dff784e7cd5e8
Marta Fonda <martafondapascual@gmail.com>
2015-12-22 19:21:31 +0100

    Guess a number between 1 and 5 application interface and main functionallity

--- gessTheNumber/ViewController.swift
+++ gessTheNumber/ViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberInput: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func sendButton(sender: UIButton) {
+        var inputValue = numberInput.text
+        var randomNumber = arc4random_uniform(4)
+        randomNumber = randomNumber + 1
+        
+        if Int(inputValue!) == Int(randomNumber){
+            numberLabel.text = "CORRECT!!"
+        } else {
+            numberLabel.text = "INCORRECT :("
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
